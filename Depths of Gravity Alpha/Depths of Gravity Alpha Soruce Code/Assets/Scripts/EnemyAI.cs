a7e6156d184504d77910786e7e2e64f52b5e9215
Liam Francisco <liamf@nevada.unr.edu>
2023-05-01 11:15:11 -0700

    updated ai

--- Depths of Gravity Alpha/Depths of Gravity Alpha Soruce Code/Assets/Scripts/EnemyAI.cs
+++ Depths of Gravity Alpha/Depths of Gravity Alpha Soruce Code/Assets/Scripts/EnemyAI.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    //world variables
     public GameObject player;
     public GameObject projectilePrefab;
     public AudioSource spitSound;
-    public int projectileSpeed;
 
+    //projectile variables
+    public int projectileSpeed;
     GameObject enemyProjectile;
     Vector3 distance;
     float shootTimer;
     float bufferTime;
-    
+
+    //movement variables
+    float lerpTimer = 0.0f;
+    public float oneWayTime;
+    public Vector3 initialPos;
+    public Vector3 finalPos;
+    bool initToFin;
+    Quaternion initRotation;
+
+
     // Start is called before the first frame update
     void Start()
     {
         shootTimer = 0;
         bufferTime = Random.Range(0.5f, 1f);
+        initToFin = true;
+        initRotation = transform.rotation;
     }
 
     // Update is called once per frame
@@ -27,10 +40,12 @@ public class EnemyAI : MonoBehaviour
     {
         distance = transform.position - player.transform.position;
 
-        if(distance.magnitude < 7.5)
+        if (distance.magnitude < 7.5)
         {
-            transform.LookAt(player.transform);
-            if(shootTimer == bufferTime)
+            var targetRotation = Quaternion.LookRotation(player.transform.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5 * Time.deltaTime);
+
+            if (shootTimer == bufferTime)
             {
                 spitSound.Play();
                 enemyProjectile = Instantiate(projectilePrefab, transform.position + transform.forward, transform.rotation);
@@ -40,13 +55,62 @@ public class EnemyAI : MonoBehaviour
                 //spitSound.Stop();
             }
         }
+        else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, initRotation, 1 * Time.deltaTime);
+        }
 
         shootTimer += Time.deltaTime;
 
-        if(shootTimer > bufferTime)
+        if (shootTimer > bufferTime)
         {
             shootTimer = bufferTime;
         }
 
+        MoveBetweenPositions();
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player Projectile"))
+            Destroy(gameObject);
+    }
+
+    void MoveBetweenPositions()
+    {
+        if (lerpTimer < oneWayTime)
+        {
+            if (initToFin)
+            {
+                float newX = Mathf.SmoothStep(initialPos.x, finalPos.x, lerpTimer / oneWayTime);
+                float newY = Mathf.SmoothStep(initialPos.y, finalPos.y, lerpTimer / oneWayTime);
+                float newZ = Mathf.SmoothStep(initialPos.z, finalPos.z, lerpTimer / oneWayTime);
+                transform.position = new Vector3(newX, newY, newZ);
+            }
+            else
+            {
+                float newX = Mathf.SmoothStep(finalPos.x, initialPos.x, lerpTimer / oneWayTime);
+                float newY = Mathf.SmoothStep(finalPos.y, initialPos.y, lerpTimer / oneWayTime);
+                float newZ = Mathf.SmoothStep(finalPos.z, initialPos.z, lerpTimer / oneWayTime);
+                transform.position = new Vector3(newX, newY, newZ);
+            }
+
+            lerpTimer += Time.deltaTime;
+        }
+        else
+        {
+            lerpTimer = 0f;
+            if (initToFin)
+            {
+                initToFin = false;
+            }
+            else
+            {
+                initToFin = true;
+            }
+        }
     }
 }
+
+
